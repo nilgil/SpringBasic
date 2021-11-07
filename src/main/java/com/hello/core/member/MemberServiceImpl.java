1b951490e8a383998f5f635fe85364e391a22e93
0giri <higiri@naver.com>
2021-11-08 01:09:01 +0900

    Initial commit

---
+++ src/main/java/com/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package com.hello.core.member;
+
+import org.springframework.stereotype.Service;
+
+public class MemberServiceImpl implements MemberService {
+    private MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
