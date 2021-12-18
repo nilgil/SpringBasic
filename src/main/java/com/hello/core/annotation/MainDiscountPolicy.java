72b2c1366e442078fd2722397a4ca919b23b2d8a
0giri <higiri@naver.com>
2021-12-19 03:57:42 +0900

    ~프로토타입 스코프 싱글톤빈과 함께 사용시 문제점

---
+++ src/main/java/com/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,13 @@
+package com.hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+}
---
+++ src/test/java/com/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,47 @@
+package com.hello.core.autowired;
+
+import com.hello.core.AutoAppConfig;
+import com.hello.core.discount.DiscountPolicy;
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        int fisDiscountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        assertThat(fisDiscountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
